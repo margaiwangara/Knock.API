475e83961e0347e6c9668917fe15f513afbf70a3
margaiwangara <margaiwangara@gmail.com>
2020-02-17 17:24:56 +0300

    added data annotations to restaurantforcreationdto for validation

--- Models/RestaurantForCreationDto.cs
+++ Models/RestaurantForCreationDto.cs
@@ -1,9 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Knock.API.Models
 {
   public class RestaurantForCreationDto
   {
+    [Required(ErrorMessage="Name field is required")]
+    [MaxLength(100, ErrorMessage="Name field maximum length is 100 chars")]
     public string Name { get; set; }
+    
+    [Required(ErrorMessage="Address field is required")]
+    [MaxLength(100, ErrorMessage="Address field maximum length is 100 chars")]
     public string Address { get; set; }
+    
+    [MaxLength(100, ErrorMessage="Website field maximum length is 100 chars")]
     public string Website { get; set; }
     public bool OffersTakeout { get; set; }
   }
