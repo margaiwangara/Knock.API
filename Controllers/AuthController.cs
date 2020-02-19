0e755d66141837e04b640081b42309b481c643aa
margaiwangara <margaiwangara@gmail.com>
2020-02-19 17:33:12 +0300

    split token generation into a private function for multiple use in login and registration

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -44,24 +44,33 @@ namespace Knock.API.Controllers
         return Unauthorized(new { message = "Invalid Email or Password" });
       }
 
-      var tokenHandler = new JwtSecurityTokenHandler();
-      byte [] key = Encoding.ASCII.GetBytes("randomstuff");
-      var tokenDescriptor = new SecurityTokenDescriptor 
-      { 
-        Subject = new ClaimsIdentity(
-          new Claim[] 
-          {
-            new Claim(ClaimTypes.Name, user.Id.ToString())
-          }),
-          Expires = DateTime.UtcNow.AddDays(7),
-          SigningCredentials  = new SigningCredentials(new SymmetricSecurityKey(key), 
-                                    SecurityAlgorithms.HmacSha256Signature)
-      };
-
-      var token = tokenHandler.CreateToken(tokenDescriptor);
-      var tokenString = tokenHandler.WriteToken(token);
+      string token = GenerateAuthToken(user);
 
       return Ok(new { Token = token, Id = user.Id });
     }
+
+    private string GenerateAuthToken(UserForRegistrationDto user)
+    {
+      var tokenHandler = new JwtSecurityTokenHandler();
+        byte [] key = Encoding.ASCII.GetBytes("randomstuff");
+        var tokenDescriptor = new SecurityTokenDescriptor 
+        { 
+          Subject = new ClaimsIdentity(
+            new Claim[] 
+            {
+              new Claim(ClaimTypes.Name, user.Id.ToString())
+            }),
+            Expires = DateTime.UtcNow.AddDays(7),
+            SigningCredentials  = new SigningCredentials(new SymmetricSecurityKey(key), 
+                                      SecurityAlgorithms.HmacSha256Signature)
+        };
+
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        var tokenString = tokenHandler.WriteToken(token);
+
+        return tokenString;
+    }
+    
   }
+  
 }
