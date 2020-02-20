d1f2ed1154bce587bdfa83038c5588ff6a2d442d
margaiwangara <margaiwangara@gmail.com>
2020-02-20 17:02:17 +0300

    added secret key to appsettings, added auth code to startup file (untested)

--- Startup.cs
+++ Startup.cs
@@ -7,17 +7,25 @@ using Knock.API.DbContexts;
 using Microsoft.EntityFrameworkCore;
 using Knock.API.Services;
 using  AutoMapper;
+using Knock.API.Helpers;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System;
+using System.Threading.Tasks;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Knock.API
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
             Configuration = configuration;
+            _env = env;
         }
 
         public IConfiguration Configuration { get; }
+        private readonly IWebHostEnvironment _env;
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -27,13 +35,56 @@ namespace Knock.API
             })
             .AddXmlDataContractSerializerFormatters();
             
-            services.AddScoped<IKnockRepository, KnockRepository>();
-            
-            services.AddAutoMapper(typeof(Startup));
-            
             services.AddDbContext<KnockContext>(options => {
                 options.UseSqlite(Configuration.GetConnectionString("KnockContext"));
             });
+
+            services.AddScoped<IKnockRepository, KnockRepository>();
+            services.AddAutoMapper(typeof(Startup));
+            
+            // auth code
+            services.AddCors();
+            var appSettingsSection = Configuration.GetSection("AppSettings");
+            services.Configure<AppSettings>(appSettingsSection);
+            
+            var appSettings = appSettingsSection.Get<AppSettings>();
+            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+            
+            services.AddAuthentication(a => 
+            {
+                a.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                a.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(a => 
+            {
+                a.Events = new JwtBearerEvents
+                {
+                    OnTokenValidated = context => 
+                    {
+                        var userService = context.HttpContext.RequestServices.GetRequiredService<IKnockRepository>();
+                        var userId = Guid.Parse(context.Principal.Identity.Name);
+                        var user = userService.GetUserAsync(userId);
+
+                        if(user == null)
+                        {
+                            context.Fail("Unauthorized Access");
+                        }
+
+                        return Task.CompletedTask;
+                    }
+                };
+                a.RequireHttpsMetadata = false;
+                a.SaveToken = true;
+                a.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false                   
+                };
+            });
+            // end of auth code
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,6 +99,14 @@ namespace Knock.API
 
             app.UseRouting();
 
+            // enable cors
+            app.UseCors(x => x
+                    .AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+            
+            // enable auth
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
