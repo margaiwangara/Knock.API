06c2f3c8b8a1c1cf50b7ceca4a7df9ae2263bb5b
margaiwangara <margaiwangara@gmail.com>
2020-02-22 15:42:06 +0300

    modified restaurant model, added average-rating column

--- Entities/Restaurant.cs
+++ Entities/Restaurant.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Knock.API.Entities
 {
@@ -21,6 +22,9 @@ namespace Knock.API.Entities
     #nullable enable
     public string? Website { get; set; }
     public bool OffersTakeout { get; set; }
+
+    [Column(TypeName = "decimal(2,2)")]
+    public double AverageRating { get; set; } = 0.0;
     public DateTimeOffset DateCreated { get; set; }
         = DateTimeOffset.Now;
     public ICollection<Review> Reviews { get; set; }
---
+++ Migrations/20200222124033_AddedAverageRatingColumnToRestaurantTable.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Knock.API.Migrations
+{
+    public partial class AddedAverageRatingColumnToRestaurantTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "AverageRating",
+                table: "Restaurants",
+                type: "decimal(2,2)",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AverageRating",
+                table: "Restaurants");
+        }
+    }
+}
